f0636ec7cd65d6d1f89bc58012241ae2faa24989
objectified <objectified@gmail.com>
2013-01-22 14:04:58 +0100

    initial version

---
+++ LogTailMinutes.php
@@ -0,0 +1,68 @@
+<?php
+class LogTailMinutes {
+    private $logfile;
+    private $date_format;
+    private $minutes_back;
+    private $egrep_location;
+    private $start_time;
+    private $filter_regex;
+
+    public function __construct(
+            $logfile, $date_format, $minutes_back, 
+            $start_time = 'now', $egrep_location = '/bin/egrep'
+    ) {
+        $this->logfile = $logfile;
+        $this->date_format = $date_format;
+        $this->minutes_back = $minutes_back;
+        $this->start_time = $start_time;
+        $this->egrep_location = $egrep_location;
+    }
+
+    public function buildEgrepRegex() {
+        $intervals = array();
+        $start = strtotime($this->start_time);            
+
+        for($i = 0; $i <= $this->minutes_back; $i++) {
+            $intervals[] = date(
+                $this->date_format, 
+                strtotime("$i minutes ago", $start)
+            );
+        }
+
+        return "'".join('|', $intervals)."'";
+    }
+
+    public function setFilterRegex($filter_regex) {
+        $this->filter_regex = $filter_regex;
+    }
+
+    public function buildEgrepCmd() {
+        $egrep_args = '-h';
+        $regex = $this->buildEgrepRegex();
+
+        $cmd = join(' ', array(
+                $this->egrep_location, 
+                $egrep_args, 
+                $regex, 
+                $this->logfile
+            )
+        );
+
+        if(isset($this->filter_regex)) {
+            $cmd .= ' | ' . join(' ', array(
+                    $this->egrep_location, 
+                    $egrep_args, 
+                    "'".$this->filter_regex."'"
+                )
+            );
+        }
+
+        return $cmd;
+    }
+
+    public function getLines() {
+        $cmd = $this->buildEgrepCmd();
+
+        return explode("\n", shell_exec($cmd));
+    }
+}
--- README.md
+++ README.md
@@ -1,4 +1,20 @@
 logtailminutes
 ==============
 
-Dead simple, small utility to extract lines from log files based on time range
+Dead simple, tiny utility to extract lines from log files based on time range. Usage:
+
+    $tail = new LogTailMinutes(
+        '/path/to/logfile',  // log file to extract files from
+        'd/m/Y',            // date format used in the log file, see php.net/date
+        10,                 // how many minutes to go back
+        'now',              // time expression to start from, see php.net/strtotime
+        '/path/to/egrep'    // path to egrep binary
+    );
+
+    // optionally, set a filter on the matching results (egrep compatible regex)
+    $tail->setFilterRegex('HTTP/1.1" (4|5)[0-9]{2}');
+
+    // get matched lines
+    $lines = $tail->getLines();
+
+
